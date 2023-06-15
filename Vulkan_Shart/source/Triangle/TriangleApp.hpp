da27d48c8261eb2c7a01d2098515f5f3f8689fee
ItsDuska <89298953+ItsDuska@users.noreply.github.com>
2023-06-15 22:22:23 +0300

    Day 1 of poking my brain with a stick called Vulkan.

--- Vulkan_Shart/source/Triangle/TriangleApp.hpp
+++ Vulkan_Shart/source/Triangle/TriangleApp.hpp
@@ -1,19 +1,64 @@
 #pragma once
 
-
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
+#include <string.h>
+#include <optional>
+#include <set>
+#include <cstdint>
+#include <limits>
+#include <algorithm>
+
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif 
 
 /*
 	vta = VULKAN TRIANGLE APPLICATION
 */
+
 namespace vta
 {
+	struct QueueFamilyIndices
+	{
+		std::optional<uint32_t> graphicsFamily;
+		std::optional<uint32_t> presentFamily;
+
+		bool isComplete();
+	};
+
+	struct SwapChainSupportDetails
+	{
+		VkSurfaceCapabilitiesKHR capabilities;
+		std::vector<VkSurfaceFormatKHR> formats;
+		std::vector<VkPresentModeKHR> presentModes;
+	};
+
+	const std::vector<const char*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+	};
+
+	const std::vector<const char*> deviceExtensions = {
+	VK_KHR_SWAPCHAIN_EXTENSION_NAME
+	};
+
+	VkResult CreateDebugUtilsMessengerEXT(
+		VkInstance instance,
+		const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+		const VkAllocationCallbacks* pAllocator,
+		VkDebugUtilsMessengerEXT* pDebugMessenger);
+
+	void DestroyDebugUtilsMessengerEXT(VkInstance instance,
+		VkDebugUtilsMessengerEXT debugMessenger,
+		const VkAllocationCallbacks* pAllocator);
+
 	class TriangleApp 
 	{
 	public:
@@ -26,10 +71,50 @@ namespace vta
 		void cleanup();
 
 		void createInstance();
+		void extensionsSupported();
+		bool checkValidationLayerSupport();
+		std::vector<const char*> getRequiredExtensions();
+		void setupDebugMessenger();
+		void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
 
+		void pickPhysicalDevice();
+		bool isDeviceSuitable(VkPhysicalDevice device);
+
+		void createLogicalDevice();
+		void createSurface();
+
+		bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+
+		QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+		SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+		VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+		VkPresentModeKHR chooseSwapPresentMode(const std::vector < VkPresentModeKHR>& availablePresentModes);
+		VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+		void createSwapChain();
+
+		void createImageViews();
+
+		static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+			VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+			VkDebugUtilsMessageTypeFlagsEXT messageType,
+			const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+			void* pUserData);
 	private:
 		GLFWwindow *window = nullptr;
 		VkInstance instance;
+		VkDebugUtilsMessengerEXT debugMessenger;
+		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+		VkDevice device;
+		VkQueue graphicsQueue;
+		VkSurfaceKHR surface;
+		VkQueue presentQueue;
+		VkSwapchainKHR swapChain;
+
+		std::vector<VkImage> swapChainImages;
+		VkFormat swapChainImageFormat;
+		VkExtent2D swapChainExtent;
+
+		std::vector<VkImageView> swapChainImageViews;
 
 		const int WINDOW_WIDTH = 800;
 		const int WINDOW_HEIGHT = 600;
