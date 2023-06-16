0087dcc28bf1ffc1fd864924094ee9cfd3ea56a8
ItsDuska <89298953+ItsDuska@users.noreply.github.com>
2023-06-16 19:53:15 +0300

    Triangle works!

--- Vulkan_Shart/source/Triangle/TriangleApp.hpp
+++ Vulkan_Shart/source/Triangle/TriangleApp.hpp
@@ -13,6 +13,7 @@
 #include <cstdint>
 #include <limits>
 #include <algorithm>
+#include <fstream>
 
 #ifdef NDEBUG
 	const bool enableValidationLayers = false;
@@ -49,6 +50,8 @@ namespace vta
 	VK_KHR_SWAPCHAIN_EXTENSION_NAME
 	};
 
+	const int MAX_FRAMES_IN_FLIGHT = 2;
+
 	VkResult CreateDebugUtilsMessengerEXT(
 		VkInstance instance,
 		const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
@@ -59,6 +62,9 @@ namespace vta
 		VkDebugUtilsMessengerEXT debugMessenger,
 		const VkAllocationCallbacks* pAllocator);
 
+	static std::vector<char> readFile(const std::string& filename);
+
+
 	class TriangleApp 
 	{
 	public:
@@ -66,57 +72,126 @@ namespace vta
 
 	private:
 		void initWindow();
+
 		void initVulkan();
+
 		void mainLoop();
+
 		void cleanup();
 
 		void createInstance();
+
 		void extensionsSupported();
+
 		bool checkValidationLayerSupport();
+
 		std::vector<const char*> getRequiredExtensions();
 		void setupDebugMessenger();
+
 		void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
 
 		void pickPhysicalDevice();
+
 		bool isDeviceSuitable(VkPhysicalDevice device);
 
 		void createLogicalDevice();
+
 		void createSurface();
 
 		bool checkDeviceExtensionSupport(VkPhysicalDevice device);
 
 		QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+
 		SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+
 		VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+
 		VkPresentModeKHR chooseSwapPresentMode(const std::vector < VkPresentModeKHR>& availablePresentModes);
+
 		VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+
 		void createSwapChain();
 
 		void createImageViews();
+
+		void createGraphicsPipeLine();
+
+		VkShaderModule createShaderModule(const std::vector<char>& code);
+
+		void createRenderPass();
+
+		void createFramebuffers();
+
+		void createCommandPool();
+
+		void createCommandBuffers();
+
+		void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
+
+		void drawFrame();
+
+		void createSyncObjects();
+
+		void recreateSwapChain();
+
+		void cleanupSwapChain();
+
+		static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
 
 		static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
 			VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
 			VkDebugUtilsMessageTypeFlagsEXT messageType,
 			const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
 			void* pUserData);
+
 	private:
 		GLFWwindow *window = nullptr;
+
 		VkInstance instance;
+
 		VkDebugUtilsMessengerEXT debugMessenger;
+
 		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
 		VkDevice device;
+
 		VkQueue graphicsQueue;
+
 		VkSurfaceKHR surface;
+
 		VkQueue presentQueue;
+
 		VkSwapchainKHR swapChain;
 
 		std::vector<VkImage> swapChainImages;
+
 		VkFormat swapChainImageFormat;
+
 		VkExtent2D swapChainExtent;
 
 		std::vector<VkImageView> swapChainImageViews;
 
+		VkRenderPass renderPass;
+
+		VkPipelineLayout pipelineLayout;
+
+		VkPipeline graphicsPipeline;
+
+		std::vector<VkFramebuffer> swapChainFramebuffers;
+
+		VkCommandPool commandPool;
+
+		std::vector<VkCommandBuffer> commandBuffers;
+
+		std::vector<VkSemaphore> imageAvailableSemaphores;
+		std::vector<VkSemaphore> renderFinishedSemaphores;
+		std::vector<VkFence> inFlightFences;
+
+		bool framebufferResized = false;
+
 		const int WINDOW_WIDTH = 800;
 		const int WINDOW_HEIGHT = 600;
+
+		uint32_t currentFrame = 0;
 	};
 }
